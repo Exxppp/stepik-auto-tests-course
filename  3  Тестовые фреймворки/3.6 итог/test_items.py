7b7b4e59b7af7667d050ebb0335e1f5249f87e30
Exxppp <85801190+Exxppp@users.noreply.github.com>
2022-09-11 11:42:12 +0300

    Delete  3  Тестовые фреймворки/3.6 итог directory

---  3  Тестовые фреймворки/3.6 итог/test_items.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-import time
-
-
-def test_find_add_busket(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
-    time.sleep(2)
